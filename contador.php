2cd283aa135500f777922681d878b4d108479ced
manuvgmim <manu.vgim04@gmail.com>
2024-09-23 18:21:27 +0200

    Contador actualizado

--- contador.php
+++ contador.php
@@ -6,9 +6,10 @@
     <title>Contador</title>
 </head>
 <body>
+    <h1>Contadores</h1>
     <?php
     
-
+    echo"<h2>Bucle for</h2>";
     for ($i=1; $i <= 100; $i++) { 
         if($i == 100){
             echo"$i<br>";
@@ -17,10 +18,11 @@
         }
     }
 
+    echo"<h2>Bucle while</h2>";
     $num = 10;
     while($num >= 0 ){
         if($num == 0){
-            echo"$num";
+            echo$num;
         }else{
             echo"$num-";
             

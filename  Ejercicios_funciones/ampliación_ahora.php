982c9965f23a3819db6eb857d897049746bfabc7
manuvgmim <manu.vgim04@gmail.com>
2024-09-30 16:41:31 +0200

    Ampliación 7 casi hecha

---
+++  Ejercicios_funciones/ampliación_ahora.php
@@ -0,0 +1,31 @@
+<!DOCTYPE html>
+<html lang="es">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Ampliación</title>
+</head>
+<body>
+    
+    <?php
+    
+        function calculoTiempo($segundos){
+            $anyo = ($segundos/60/60/24/365);
+            $meses = ($segundos/60/60/24/30);
+            $dias = ($segundos/60/60/24);
+
+            return "Has vivido una cantidad de $anyo años, $meses meses, $dias dias.";
+        }
+
+        date_default_timezone_set('Europe/Madrid');
+
+        $fechaActual = time();
+        $fechaD = strtotime("28/10/2004");
+        
+        $segundos = $fechaActual - $fechaD;
+
+        echo $segundos, calculoTiempo($segundos);
+    ?>
+
+</body>
+</html>

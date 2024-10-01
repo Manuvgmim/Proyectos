0c1896916d1ea65454b1a742248d9e35cd4d1a84
manuvgmim <manu.vgim04@gmail.com>
2024-10-01 17:07:25 +0200

    ejercicios arrays

---  Ejercicios_funciones/Ejercicio9/copia_seguridad.php
+++  Ejercicios_funciones/Ejercicio9/copia_seguridad.php
@@ -6,13 +6,13 @@
     <title>Copia seguridad</title>
 </head>
 <body>
-    <?php 
+    <?php
         $fichero = "datos.txt";
         $ficherocopia = "copia_datos.txt";
         if(file_exists($fichero)){
             $contenido = file_get_contents($fichero);
 
-            $contenidoModificado = preg_replace("/^\b\w{3,}@\w{3,}\d[A-Za-z]{2,4}$/","Fran@gmail.com",$contenido);
+            $contenidoModificado = preg_replace("/^\w{3,}+@\w{3,}+.\d[A-Za-z]{2,}$/","Fran@gmail.com",$contenido);
 
             file_put_contents($ficherocopia,$contenidoModificado);
             
---
+++ Ejercicios_arrays/tabla_multiplicar.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang="es">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Tabla de multiplicar</title>
+</head>
+<body>
+    <?php 
+        $numeros = array();
+
+        for($i=0;$i<=9;$i++){
+            for($j=0;$j<=9;$j++)
+        }
+        
+    ?>
+</body>
+</html>
